755bd2a7471395062fb697222f12c317badc7438
Rajesh <rjshpattanaik1@gmail.com>
2021-06-24 16:41:13 +0530

    used Pug template engine

--- index.js
+++ index.js
@@ -16,13 +16,22 @@ const port = process.env.PORT || "8000";
  * App Configuration
  */
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
 /**
  * Routes Definitions
  */
 
 app.get("/", (req, res) => {
-    res.status(200).send("WHATABYTE: Food For Devs")
-})
+    //res.status(200).send("WHATABYTE: Food For Devs");
+    res.render("index", { title: "Home" });
+});
+
+app.get("/user", (req, res) => {
+    res.render("user", { title: "Profile", userProfile: { nickname: "Klaus" } });
+});
 
 /**
  * Server Activation
